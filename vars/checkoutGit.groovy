a484c048b81bc4cbf99ed4e23f78b7e62d521aa7
vamshikrishna <86669609+vamshiyekbote@users.noreply.github.com>
2023-10-19 14:23:22 +0530

    Delete vars/checkoutGit.groovy

--- vars/checkoutGit.groovy
+++
@@ -1,8 +0,0 @@
-def call(String gitUrl, String gitBranch) {
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name: main]],
-        userRemoteConfigs: [[url: https://github.com/vamshiyekbote/Jenkins_shared_library.git 
-        ]]]
-    )
-}
